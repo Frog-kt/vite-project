4875e5594968bf41674e3c5adefeb1d716edd241
Frog-kt <kasuo.takumi@gmail.com>
2021-03-05 16:02:17 +0900

    chore: Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div className="flex-auto bg-white rounded-tl-xl border-l shadow-xl">
-
+    <div className="flex flex-row flex-auto bg-white rounded-tl-xl border-l shadow-xl">
+      <div className="w-1/5">left</div>
+      <div className="w-3/5">center</div>
+      <div className="w-1/5">right</div>
     </div>
   )
 }
